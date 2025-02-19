ba318311cceefb457ebf370ab2f62ffec6e0db7c
Woody <waiyanwoody@gmail.com>
2025-02-19 23:24:18 +0630

    layout update

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,3 +1,4 @@
+import TextInput from '@/Components/TextInput';
 import { usePage } from '@inertiajs/react';
 import React, { useEffect, useState } from 'react'
 
@@ -15,6 +16,17 @@ const ChatLayout = ({ children }) => {
     console.log('conversations', conversations);
     console.log('selectedConversation', selectedConversation);
 
+    const onSearch = (ev) => {
+        const search = ev.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return (
+                    conversation.name.toLowerCase().includes(search)
+                )
+            })
+        )
+    }
+
     useEffect(() => {
         setSortedConversations(
             localConversations.sort((a, b) => {
@@ -25,7 +37,7 @@ const ChatLayout = ({ children }) => {
                 } else if (b.blocked_at) {
                     return -1;
                 }
-                if(a.last_message_date  && b.last_message_date) {
+                if (a.last_message_date && b.last_message_date) {
                     return b.last_message_date.localCompare(
                         a.last_message_date
                     )
@@ -42,7 +54,7 @@ const ChatLayout = ({ children }) => {
 
     useEffect(() => {
         setLocalConversations(conversations);
-    },[localConversations])
+    }, [localConversations])
 
     useEffect(() => {
         Echo.join('online')
@@ -76,10 +88,56 @@ const ChatLayout = ({ children }) => {
     }, []);
 
     return (
-        <div>
-            Chat Layout
+        <>
+            <div className="flex flex-1 w-full overflow-hidden">
+                <div className={`
+                    transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800
+                    flex flex-col overflow-hidden
+                    ${selectedConversation ? "-ml-[100%] sm:ml-0" : ""
+                    }
+                    `}>
+                    <div className="flex items-center justify-between py-2 px-3 text-xl font-medium">
+                        My conversations
+                        <div
+                            className="tooltip tooltip-left"
+                            data-tip="Create new Group"
+                        >
+                            <button
+                                onClick={(ev)=>setShowGroupModal(true)}
+                                className="text-gray-400 hover:text-gray-200">
+                                <PencilSquareIcon className="w-4 h-4 inline-block ml-2" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="p-3">
+                        <TextInput
+                            onKeyUp={onSearch}
+                            placeholder="Filter users and groups"
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="flex-1 overflow-auto">
+                        {sortedConversations &&
+                            sortedConversations.map((conversation) => (
+                            <ConversationItem
+                                    key={`${
+                                        conversation.is_group
+                                        ? "group_"
+                                        : "user_"
+                                        }${conversation.id}`}
+                                    conversation={conversation}
+                                    online={!!isUserOnline(conversation.id)}
+                                    selectedConversation={selectedConversation}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="flex flex-1 flex-col overflow-hidden">
+                    {children}
+                </div>
+            </div>
 
-        </div>
+        </>
     )
 }
 
